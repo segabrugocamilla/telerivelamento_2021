fb41a8a12a3a995135817b90b4ca4984b4cf7e43
segabrugocamilla <80909068+segabrugocamilla@users.noreply.github.com>
2021-03-18 22:07:55 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -6,4 +6,20 @@ library(raster)
 
 p224r63_2011<-brick("p224r63_2011_masked.grd")
 # p224r63_2011
-plot(p224r63_2011) #visualizzare tutte le bande
+
+plot(p224r63_2011) #visualizzare tutte le bande con valori riflettanza
+
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: NIR
+# B5: infrarosso intermedio
+# B6: infrarosso termico
+
+#colorRampPalette per cambiare i colori
+cl<-colorRampPalette(c("black","grey","light gray"))(100) #c è il vettore argomento, 100 sono i livelli del colore
+
+plot(p224r63_2011, col=cl)
+
+# cambiamo colore a piacere
+cl<-colorRampPalette(c("green","purple","orange","pink"))(200)
