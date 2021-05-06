85ceba643141639fd0ed32df019aaf301415d8bb
segabrugocamilla <80909068+segabrugocamilla@users.noreply.github.com>
2021-05-06 10:42:43 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,3 +3,5 @@
 setwd("C:/lab/") #settiamo la working directory, da cui prenderemo il nostro codice originale per fare il "report"
 
 library(knitr)
+
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr")) #utiliziammo la funzione stitch per andare a prendere il codice
