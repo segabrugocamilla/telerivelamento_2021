2b4b60a901a2e9aef693ffc33377ff444db46a2b
segabrugocamilla <80909068+segabrugocamilla@users.noreply.github.com>
2021-05-10 11:38:22 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -13,7 +13,7 @@ plotRGB(so, 1,2,3, stretch="lin")
 
 # Classificazione non supervisionata, senza impatto dell'utente i pixel vengono associati per il valore in RGB
 soc <- unsuperClass(so, nClasses=3) # ha creato in uscita il modello e la mappa
-plot(soc$map) # facciamo plot dell'immagine classificata e in particolare della mappa
+plot(soc$map) # facciamo plot dell'immagine classificata e visto che vogliamo vedere la mappa la leghiamo a quest'ultima
 
 # Classificazione non supervisionata con 20 classi
 sot <- unsuperClass(so, nClasses=20)
@@ -25,4 +25,22 @@ sun <- brick("sun.png")
 
 # Classificazione non supervisionata
 sunc <- unsuperClass(sun, nClasses=3)
-plot(sunc$map)
+plot(sunc$map, col=cl)
+
+cl <- colorRampPalette(c('yellow','purple','pink'))(100)
+plot(sunc$map, col=cl)
+
+# Gran Canyon 
+# https://landsat.visibleearth.nasa.gov/view.php?id=80948
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
+
+gcc4 <- unsuperClass(gc, nClasses=4)
+plot(gcc4$map)
