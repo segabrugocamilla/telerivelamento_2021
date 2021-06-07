3a8947be61a41751f19bf542c4e6c1bc9cc1c6f6
segabrugocamilla <80909068+segabrugocamilla@users.noreply.github.com>
2021-06-07 17:26:16 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,13 +1,18 @@
 # il mio primo codice in R per il corso di telerilevamento
-setwd("C:/lab/")
-
+# per prima cosa carichiamo e richiamiamo le librerie
 # install.packages("raster")
 library(raster)
+library(sp) # per la gestione dei dati all'internod el software
+# install.packages("RStoolbox")
+library(RStoolbox)
 
-p224r63_2011<-brick("p224r63_2011_masked.grd") # questa funzione importa un intero set di dati (nel nostro caso bande) creando un RasterBrick.
+
+setwd("C:/lab/") # settiamo la set working director
+
+p224r63_2011<-brick("p224r63_2011_masked.grd") # brick è la funzione per creare un raster multistrato: importa un intero set di dati (nel nostro caso bande dai satelliti) creando un RasterBrick. Asocciamo il nome "p224r63_2011"
 # p224r63_2011
 
-plot(p224r63_2011) #visualizzare tutte le bande con valori riflettanza
+plot(p224r63_2011) # utilizziamo la funzione plot per visualizzare tutte le bande con i valori di riflettanza
 
 # Bande Landsat
 # B1: blu
@@ -20,17 +25,18 @@ plot(p224r63_2011) #visualizzare tutte le bande con valori riflettanza
 
 ### DAY 2
 #colorRampPalette per cambiare i colori
-cl<-colorRampPalette(c("black","grey","light gray"))(100) #c è il vettore argomento, 100 sono i livelli del colore
+cl<-colorRampPalette(c("black","grey","light gray"))(100) # c è il vettore argomento, 100 sono i livelli del colore
 
-plot(p224r63_2011, col=cl)
+plot(p224r63_2011, col=cl) # plottiamo con i nuovi colori
 
 # cambiamo colore a piacere
 cl<-colorRampPalette(c("green","purple","orange","pink"))(200)
 
 ### DAY 3
-plot(p224r63_2011$B1_sre) 
+# utilizziamo il simbolo "$" per legale una banda all'immagine satellitare - scelgo il livello del multistrato che ci interessa
+plot(p224r63_2011$B1_sre)
 
-# plot della banda uno con la scala di colori che voglio
+# plot della banda uno con la scala di colori che vogliamo
 
 cl<-colorRampPalette(c("green","purple","orange","pink"))(200)
 plot(p224r63_2011$B1_sre)
@@ -38,13 +44,14 @@ plot(p224r63_2011$B2_sre)
 
 dev.off() #per chiudere i grafici
 
-#plot di diverse bande all'interno di uno stesso grafico
-# 1 riga, 2 colonne
+# par funzione generica, serve per fare un settaggio dei parametri grafici di un grafico che si vuole creare 
+# multiframe - plot di diverse bande all'interno di uno stesso grafico
+# multiframe : 1 riga, 2 colonne
 par(mfrow=c(1,2))
 plot(p224r63_2011$B1_sre)
 plot(p224r63_2011$B2_sre)
 
-# 2 row, 1 columns
+# 2 righe, 1 colonne
 par(mfrow=c(2,1))
 plot(p224r63_2011$B1_sre)
 plot(p224r63_2011$B2_sre)
@@ -63,7 +70,7 @@ plot(p224r63_2011$B2_sre)
 plot(p224r63_2011$B3_sre)
 plot(p224r63_2011$B4_sre)
 
-par(mfrow=c(2,2)) #faccio il plot di un quadrante 2x2 delle bande B1, B2, B3, B4 cambiano i colori
+par(mfrow=c(2,2)) #faccio il plot di un quadrante 2x2 delle bande B1, B2, B3, B4 e poi cambio i colori
 
 clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
 plot(p224r63_2011$B1_sre, col=clb)
@@ -76,6 +83,8 @@ plot(p224r63_2011$B3_sre, col=clr)
 
 clnir <- colorRampPalette(c("red","orange","yellow")) (100)
 plot(p224r63_2011$B4_sre, col=clnir)
+
+# abbiamo imparato come predisporre in maniera riassuntiva le immagini, come plottarle
 
 ### DAY 4
 # Visualizing data by RGB plotting
@@ -106,7 +115,7 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 
 #montiamo queste quattro bande in un multiframe 2x2
-pdf("mio_primo_pdf_in_R.pdf")
+pdf("mio_primo_pdf_in_R.pdf") # salviamo il pdf dell'immagine
 par(mfrow=c(2,2))
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
@@ -114,7 +123,7 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") #così vedo bene la vegetazi
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
-plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") # chiamate anche funzione logistica, aumenta lo strech e la possibilità di quello che vediamo 
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") # chiamata anche funzione logistica, aumenta lo strech e la possibilità di quello che vediamo 
 
 #par colori naturali, colori con infrarosso sul verde, colori con infrarosso sul verde con funzione hist dello strech
 par(mfrow=c(3,1))
@@ -140,7 +149,7 @@ par(mfrow=c(1,2))
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 
-#hist
+# creiamo il pdf dell'analisi multitemporale
 pdf("multitemp.pdf")
 par(mfrow=c(2,2))
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
