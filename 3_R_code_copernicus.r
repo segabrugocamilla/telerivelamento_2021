7ddb9772c5a03cb76fadcfa1dcb6bf954296a9bd
segabrugocamilla <80909068+segabrugocamilla@users.noreply.github.com>
2022-07-25 12:08:15 +0200

    Update 3_R_code_copernicus.r

--- 3_R_code_copernicus.r
+++ 3_R_code_copernicus.r
@@ -2,12 +2,14 @@
 #Visualizzazione dei dati corpernicus
 #abbiamo bisogno della libreria raster e ncdf4 per leggere questo formato di dati
 
-library(raster)
+#install.packages("raster")
 # install.packages("ncdf4")
+
+
+library(raster)
 library(ncdf4)
 
 setwd("C:/lab/")
-library(raster)
 
 FAPAR <- raster("c_gls_FAPAR-RT0_202006300000_GLOBE_PROBAV_V2.0.1.nc") # importiano i dati in R e rinominiamo
 FAPAR
@@ -17,5 +19,7 @@ cl <- colorRampPalette(c('pink','red','orange','yellow'))(100)
 plot(FAPAR,col=cl)
 
 #questo dato si può ricampionare per renderlo più gestibile, prendiamo un pixel più grande e ad esempio estraiamo la media di quelli più piccoli contenuti. es. con fact=10 diminuisco di 10 volte
+#Consideriamo che abbiamo detto lineare quindi dobbiamo calcolare nel nostro caso 10x10 (quindi ogni 100 pixel ne uscirà uno che ha la media dei valori di tutti e 100)
+#Questo tipo di ricampionamento viene definito Bilineare
 FAPARes <- aggregate(FAPAR, fact=100) #ricampionamento bilineare (10000 volte il dato originale)
-plot(FAPARes, col=cl)
+plot(FAPARes, col=cl) #Qui vedremo infine il plot del file ricampionato con la nostra palette creata in precedenza.
